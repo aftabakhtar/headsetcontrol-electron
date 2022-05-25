f830357b1c7118624b6485b82b76da89ddebf991
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-25 22:43:56 +0500

    Add outlet

--- src/components/AppLayout/AppLayout.jsx
+++ src/components/AppLayout/AppLayout.jsx
@@ -1,10 +1,11 @@
 import { AppShell } from '@mantine/core';
 import { NavbarSimple } from '../NavbarSimple/NavbarSimple';
+import { Outlet } from 'react-router-dom';
 
 export const AppLayout = () => {
   return (
     <AppShell padding="md" navbar={<NavbarSimple />}>
-      {/* Your application here */}
+      <Outlet />
     </AppShell>
   );
 };
