475dab52e02e352c5fcb755d4bbbf172a3cd268c
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-25 22:43:48 +0500

    Add routes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,17 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppLayout } from '../components/AppLayout/AppLayout';
+import { Home } from '../components/Home/Home';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AppLayout />} />
+        <Route path="/" element={<AppLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/settings" element={<></>} />
+          <Route path="/equalizer" element={<></>} />
+        </Route>
       </Routes>
     </Router>
   );
