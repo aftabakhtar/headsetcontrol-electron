07ab0a06b0181098e635ba62fc39b82cef64e2a4
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-26 18:11:47 +0500

    Add theming and route settings component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,18 +1,29 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { AppLayout } from '../components/AppLayout/AppLayout';
 import { Home } from '../components/Home/Home';
+import { MantineProvider } from '@mantine/core';
+import { useThemeStore } from '../stores/useThemeStore';
+import { Settings } from '../components/Settings/Settings';
 
 export default function App() {
+  const theme = useThemeStore((state) => state.theme);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/settings" element={<></>} />
-          <Route path="/equalizer" element={<></>} />
-        </Route>
-      </Routes>
-    </Router>
+    <MantineProvider
+      theme={{ colorScheme: theme }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/equalizer" element={<></>} />
+          </Route>
+        </Routes>
+      </Router>
+    </MantineProvider>
   );
 }
