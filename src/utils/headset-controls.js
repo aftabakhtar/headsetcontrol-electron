17fecf25b205f0fd65e78ab3febf7b2b50b3fb6d
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-26 18:11:29 +0500

    Handle headset unavailability case

--- src/utils/headset-controls.js
+++ src/utils/headset-controls.js
@@ -1,7 +1,11 @@
-export const getHeadsetName = () => {
+export const setHeadsetStatus = (setHeadsetName) => {
   window.electron.ipcRenderer.sendMessage('ipc-example', ['status']);
   window.electron.ipcRenderer.once('ipc-example', (arg) => {
     // eslint-disable-next-line no-console
-    console.log(arg);
+    if (arg === 'Unavailable') {
+      setHeadsetName(null);
+    } else {
+      setHeadsetName(arg);
+    }
   });
 };
