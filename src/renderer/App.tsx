0a5b2f3a7ac6423920e69a498e28e6f980eb66bb
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-26 23:45:34 +0500

    Move polling code to app from home

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,8 +4,20 @@ import { Home } from '../components/Home/Home';
 import { MantineProvider } from '@mantine/core';
 import { useThemeStore } from '../stores/useThemeStore';
 import { Settings } from '../components/Settings/Settings';
+import { useInterval } from '../hooks/hooks';
+import { setBattery, setHeadsetStatus } from '../utils/headset-controls';
+import { useHeadsetStore } from '../stores/useHeadsetStore';
 
 export default function App() {
+  useInterval(() => {
+    setHeadsetStatus(setHeadsetName, setHeadsetExists);
+    setBattery(setBatteryPercentage);
+  }, 1000);
+
+  const setBatteryPercentage = useHeadsetStore((state) => state.setBattery);
+  const setHeadsetName = useHeadsetStore((state) => state.setHeadsetName);
+  const setHeadsetExists = useHeadsetStore((state) => state.setHeadsetExits);
+
   const theme = useThemeStore((state) => state.theme);
 
   return (
