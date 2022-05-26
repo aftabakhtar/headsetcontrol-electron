67a8cc02cca0a3c9880d4724248b62f272613b71
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-26 21:58:24 +0500

    Add skeleton and show setting if headset exists

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -4,10 +4,13 @@ import {
   Group,
   Paper,
   Progress,
+  SimpleGrid,
+  Skeleton,
   Text,
   ThemeIcon,
 } from '@mantine/core';
 import { BsBatteryHalf } from 'react-icons/bs';
+import { useHeadsetStore } from '../../stores/useHeadsetStore';
 
 const ICON_SIZE = 60;
 
@@ -33,65 +36,94 @@ const useStyles = createStyles((theme) => ({
 
 export const Settings = () => {
   const { classes } = useStyles();
+  const headsetExists = useHeadsetStore((state) => state.headsetExists);
 
   return (
     <>
-      <Paper radius="md" withBorder className={classes.card} mt={ICON_SIZE / 3}>
-        <ThemeIcon className={classes.icon} size={ICON_SIZE} radius={ICON_SIZE}>
-          <BsBatteryHalf size={34} />
-        </ThemeIcon>
+      {headsetExists ? (
+        <>
+          <Paper
+            radius="md"
+            withBorder
+            className={classes.card}
+            mt={ICON_SIZE / 3}
+          >
+            <ThemeIcon
+              className={classes.icon}
+              size={ICON_SIZE}
+              radius={ICON_SIZE}
+            >
+              <BsBatteryHalf size={34} />
+            </ThemeIcon>
 
-        <Text align="center" weight={700} className={classes.title}>
-          Swimming challenge
-        </Text>
-        <Text color="dimmed" align="center" size="sm">
-          32 km / week
-        </Text>
+            <Text align="center" weight={700} className={classes.title}>
+              Swimming challenge
+            </Text>
+            <Text color="dimmed" align="center" size="sm">
+              32 km / week
+            </Text>
 
-        <Group position="apart" mt="xs">
-          <Text size="sm" color="dimmed">
-            Progress
-          </Text>
-          <Text size="sm" color="dimmed">
-            62%
-          </Text>
-        </Group>
+            <Group position="apart" mt="xs">
+              <Text size="sm" color="dimmed">
+                Progress
+              </Text>
+              <Text size="sm" color="dimmed">
+                62%
+              </Text>
+            </Group>
 
-        <Progress value={62} mt={5} />
+            <Progress value={62} mt={5} />
 
-        <Group position="apart" mt="md">
-          <Text size="sm">20 / 36 km</Text>
-          <Badge size="sm">4 days left</Badge>
-        </Group>
-      </Paper>
-      <Paper radius="md" withBorder className={classes.card} mt={ICON_SIZE / 3}>
-        <ThemeIcon className={classes.icon} size={ICON_SIZE} radius={ICON_SIZE}>
-          <BsBatteryHalf size={34} />
-        </ThemeIcon>
+            <Group position="apart" mt="md">
+              <Text size="sm">20 / 36 km</Text>
+              <Badge size="sm">4 days left</Badge>
+            </Group>
+          </Paper>
+          <Paper
+            radius="md"
+            withBorder
+            className={classes.card}
+            mt={ICON_SIZE / 3}
+          >
+            <ThemeIcon
+              className={classes.icon}
+              size={ICON_SIZE}
+              radius={ICON_SIZE}
+            >
+              <BsBatteryHalf size={34} />
+            </ThemeIcon>
 
-        <Text align="center" weight={700} className={classes.title}>
-          Swimming challenge
-        </Text>
-        <Text color="dimmed" align="center" size="sm">
-          32 km / week
-        </Text>
+            <Text align="center" weight={700} className={classes.title}>
+              Swimming challenge
+            </Text>
+            <Text color="dimmed" align="center" size="sm">
+              32 km / week
+            </Text>
 
-        <Group position="apart" mt="xs">
-          <Text size="sm" color="dimmed">
-            Progress
-          </Text>
-          <Text size="sm" color="dimmed">
-            62%
-          </Text>
-        </Group>
+            <Group position="apart" mt="xs">
+              <Text size="sm" color="dimmed">
+                Progress
+              </Text>
+              <Text size="sm" color="dimmed">
+                62%
+              </Text>
+            </Group>
 
-        <Progress value={62} mt={5} />
+            <Progress value={62} mt={5} />
 
-        <Group position="apart" mt="md">
-          <Text size="sm">20 / 36 km</Text>
-          <Badge size="sm">4 days left</Badge>
-        </Group>
-      </Paper>
+            <Group position="apart" mt="md">
+              <Text size="sm">20 / 36 km</Text>
+              <Badge size="sm">4 days left</Badge>
+            </Group>
+          </Paper>
+        </>
+      ) : (
+        <SimpleGrid style={{ marginTop: '1rem' }}>
+          <Skeleton style={{ height: '90vh' }}>
+            <Text>Test</Text>
+          </Skeleton>
+        </SimpleGrid>
+      )}
     </>
   );
 };
