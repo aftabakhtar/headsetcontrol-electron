e2d9b421c2b6382cd624cbcc465cdc5b086b7448
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-26 23:45:08 +0500

    Add battery percentage checking method

--- src/utils/headset-controls.js
+++ src/utils/headset-controls.js
@@ -1,11 +1,19 @@
-export const setHeadsetStatus = (setHeadsetName) => {
-  window.electron.ipcRenderer.sendMessage('ipc-example', ['status']);
-  window.electron.ipcRenderer.once('ipc-example', (arg) => {
-    // eslint-disable-next-line no-console
+export const setHeadsetStatus = (setHeadsetName, setHeadsetExists) => {
+  window.electron.ipcRenderer.sendMessage('status', ['status']);
+  window.electron.ipcRenderer.once('status', (arg) => {
     if (arg === 'Unavailable') {
       setHeadsetName(null);
+      setHeadsetExists(false);
     } else {
       setHeadsetName(arg);
+      setHeadsetExists(true);
     }
+  });
+};
+
+export const setBattery = (setBatteryPercentage) => {
+  window.electron.ipcRenderer.sendMessage('battery', ['ping']);
+  window.electron.ipcRenderer.once('battery', (arg) => {
+    setBatteryPercentage(parseInt(arg));
   });
 };
