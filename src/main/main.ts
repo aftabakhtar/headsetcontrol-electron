4608ef47840e8a7fe6861c3d7dbbc8fa82bbec53
Aftab Akhtar <aftabakhtarnaveed@outlook.com>
2022-05-29 20:54:07 +0500

    Remove unnecessary import

--- src/main/main.ts
+++ src/main/main.ts
@@ -14,7 +14,6 @@ import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
-import { exec } from 'child_process';
 
 export default class AppUpdater {
   constructor() {
